891ebb6d14db34daf1fec9b2d274301b04a54f73
Kolade Afode <kolade.afode@yahoo.com>
2022-01-03 18:29:22 +0100

    fix(index): add techstack component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from 'next/head';
 import Layout from 'components/common/Layout';
 import About from 'components/home/About';
-import Techstack from 'components/home/Techstack';
 import Projects from 'components/home/Projects';
 import Contact from 'components/home/Contact';
+import Techstack from 'components/home/Techstack';
 
 export default function Home() {
   return (
