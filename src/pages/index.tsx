80d492b7a273d46b02bdfe4574ee5bb1e23ac49c
Kolade Afode <kolade.afode@yahoo.com>
2022-01-03 18:57:05 +0100

    refactor(Stack): create new stack component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,58 @@
-import Layout from '../../components/common/Layout';
-import About from '../../components/home/About';
-import Projects from '../../components/home/Projects';
-import Contact from '../../components/home/Contact';
-// import { Techstack } from '../../components/home/Techstack';
+import Layout from 'components/common/Layout';
+import About from 'components/home/About';
+import Projects from 'components/home/Projects';
+import Contact from 'components/home/Contact';
+import Stack from 'components/home/Stack';
 
-import styled from 'styled-components';
-import Box from '../../components/common/Container';
+// const Bold = styled.span`
+//   font-weight: 600;
+//   transition: all 0.3s ease;
+//   color: #ffffff;
+//   &:hover {
+//     color: ${(props) => props.color};
+//   }
+// `;
 
-const Bold = styled.span`
-  font-weight: 600;
-  transition: all 0.3s ease;
-  color: #ffffff;
-  &:hover {
-    color: ${(props) => props.color};
-  }
-`;
+// const Frontend = styled.p``;
 
-const Frontend = styled.p``;
+// const Backend = styled.p``;
 
-const Backend = styled.p``;
+// const Techstack = () => {
+//   return (
+//     <>
+//       <Box>
+//         <Frontend>
+//           I mainly work within the React ecosystem, but im eager to learn any
+//           other technology to solve problems. My current tech stack is{` `}
+//           <Bold color="#f7df1e ">JavaScript</Bold>,{` `}
+//           <Bold color="#007ACC">TypeScript</Bold>,{` `}
+//           <Bold>Next.js</Bold>
+//           {` `}& <Bold color="#D78CA3">Styled-Components</Bold>.{` `}
+//         </Frontend>
+//       </Box>
 
-const Techstack = () => {
-  return (
-    <>
-      <Box>
-        <Frontend>
-          I mainly work within the React ecosystem, but im eager to learn any
-          other technology to solve problems. My current tech stack is{` `}
-          <Bold color="#f7df1e ">JavaScript</Bold>,{` `}
-          <Bold color="#007ACC">TypeScript</Bold>,{` `}
-          <Bold>Next.js</Bold>
-          {` `}& <Bold color="#D78CA3">Styled-Components</Bold>.{` `}
-        </Frontend>
-      </Box>
-
-      <Box>
-        <Backend>
-          When my project needs a backend i use{` `}
-          <Bold color="#699E5B">Node js</Bold>,{` `}
-          <Bold color="#949494">Express</Bold>,{` `}
-          <Bold color="#DC5EB7">GraphQL</Bold>, &{` `}
-          <Bold color="#4DB36A">Mongo DB</Bold>
-          {` `}
-          or a GraphQL based CMS like <Bold>Graphcms</Bold>.
-        </Backend>
-      </Box>
-    </>
-  );
-};
+//       <Box>
+//         <Backend>
+//           When my project needs a backend i use{` `}
+//           <Bold color="#699E5B">Node js</Bold>,{` `}
+//           <Bold color="#949494">Express</Bold>,{` `}
+//           <Bold color="#DC5EB7">GraphQL</Bold>, &{` `}
+//           <Bold color="#4DB36A">Mongo DB</Bold>
+//           {` `}
+//           or a GraphQL based CMS like <Bold>Graphcms</Bold>.
+//         </Backend>
+//       </Box>
+//     </>
+//   );
+// };
 
 export default function Home() {
   return (
     <>
       <Layout>
         <About />
-        <Techstack />
+        <Stack />
+        {/* <Techstack /> */}
         <Projects />
         <Contact />
       </Layout>
